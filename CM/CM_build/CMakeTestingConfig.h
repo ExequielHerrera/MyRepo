779a703712ef7609f9f6ce0880a6c0b9610271ac
Exequiel Herrera De Rosa <exequiel.herrera@globant.com>
2022-09-13 09:49:16 -0300

    first push to branch main

---
+++ CM/CM_build/CMakeTestingConfig.h
@@ -0,0 +1,3 @@
+// the configured options and settings for Tutorial
+#define CmakeTesting_VERSION_MAJOR 
+#define CmakeTesting_VERSION_MINOR 0
---
+++ CM/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "CMakeTestingConfig.h"
+
+int main()
+{
+    std::cout << "Testing, CMake, one main file!\n";
+    return 0;
+}
